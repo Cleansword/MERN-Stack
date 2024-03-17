69a9c48f807fd90401210bdbe1c2712d2b9c82e3
Sagar Kumar <sagarjar213@gmail.com>
2024-03-17 17:32:26 +0530

    added

---
+++ Javascript/backend/1_file_path.js
@@ -0,0 +1,13 @@
+//working with file and path
+const fs=require("fs")
+// console.log(fs)
+console.log("starting")
+// fs.writeFileSync("sagar.txt","hello sagar, are you working with the fole and path?")  //this is not in use
+
+fs.writeFile("sagar.txt","hello sagar, are you working with the fole and path?",()=>{
+    console.log("done")
+    fs.readFile("sagar.txt",(error,data)=>{  //reading file with callback
+        console.log(error,data.toString())
+    })
+})
+console.log("ending")
